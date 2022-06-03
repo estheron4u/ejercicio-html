f6216427d09db009574edbbd200f4c04e923244d
estheron4u <esther@on4u.es>
2022-06-03 11:37:52 +0200

    add component variables

--- js/expandable.js
+++ js/expandable.js
@@ -3,7 +3,7 @@
 const expandableButton = document.querySelectorAll('.expandable__button');
 
 function toggleExpandable(event){
-        event.currentTarget.nextElementSibling.classList.toggle("expandable__element--is-expanded");
+        event.currentTarget.nextElementSibling.classList.toggle("expandable__content--is-expanded");
         event.currentTarget.classList.toggle("expandable__button--is-expanded");
 }
 
@@ -12,3 +12,8 @@ if(window.matchMedia("(max-width: 575px)").matches){
         expandableButton[i].addEventListener('click', toggleExpandable);
     }
 }
+
+// Esperar a que cargue el dom
+// Comprobar que el expandable button existe
+// Guardar cosas en variables
+// Breakpoints en variables JS
